ad6635a877bd207e219e04baab7fdacf5bbac9a6
Aster1 <gangjinsu4@gmail.com>
2023-12-07 16:35:44 +0900

    12071635

--- client/client-app/src/containers/MyPage/TeamMyRegListContainer.jsx
+++ client/client-app/src/containers/MyPage/TeamMyRegListContainer.jsx
@@ -3,38 +3,39 @@ import * as teamAppApi from '../../apis/Team/TeamAppApi'
 import TeamMyRegList from '../../components/Mypage/TeamMyRegList'
 import { useNavigate } from 'react-router-dom'
 
-const TeamMyRegListContainer = ({username}) => {
+const TeamMyRegListContainer = ({ username }) => {
 
-    
-    const [tlmList, setTlmList] = useState([])
 
-    const navi = useNavigate()
-  
-    const getTlmList = async () => {
-      const response = await teamAppApi.teamAppListByMember(username)
-      const data = await response.data
-      console.log(data)
-      setTlmList(data)
-    }
+  const [tlmList, setTlmList] = useState([])
 
-    const delHandler = async (appNo)=>{
-        const response = await teamAppApi.delApp(appNo)
-        const data = await response.data
-        navi(`/mypage/tlmList`)
-        getTlmList()
-    }
-  
-    useEffect(()=>{
-      getTlmList()
-    },[])
+  const navi = useNavigate()
 
-    const sets = {
-        delHandler,
-    }
+  const getTlmList = async () => {
+    const response = await teamAppApi.teamAppListByMember(username)
+    const data = await response.data
+    console.log(data)
+    setTlmList(data)
+  }
+
+  const delHandler = async (appNo) => {
+    if (!window.confirm(`정말로 신청을 취소하시겠습니까?`)) return
+    const response = await teamAppApi.delApp(appNo)
+    const data = await response.data
+    navi(`/mypage/tlmList`)
+    getTlmList()
+  }
+
+  useEffect(() => {
+    getTlmList()
+  }, [])
+
+  const sets = {
+    delHandler,
+  }
 
   return (
     <>
-        <TeamMyRegList sets={sets} tlmList={tlmList}></TeamMyRegList>
+      <TeamMyRegList sets={sets} tlmList={tlmList}></TeamMyRegList>
     </>
   )
 }
