d5eba8cb7cbbbb8d88824a68eb90d66966af7e95
Kimjunhyuk0320 <kimminuk0320@gmail.com>
2023-12-07 10:07:42 +0900

    병합 ^

--- client/client-app/src/App.js
+++ client/client-app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Index from './pages/Index';
 import LiveBoardList from './pages/LiveBoard/LiveBoardList';
 import LiveBoardRead from './pages/LiveBoard/LiveBoardRead';
