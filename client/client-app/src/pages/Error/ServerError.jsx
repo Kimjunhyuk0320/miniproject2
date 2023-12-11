4ba01322d474703c5173fdc1f70dee4d9cecd6ee
wwwalohacampus <h850415@naver.com>
2023-12-11 11:31:11 +0900

    ❌ 에러 처리

---
+++ client/client-app/src/pages/Error/ServerError.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../../layout/Header'
+import Footer from '../../layout/Footer'
+
+// TODO : 500 페이지 꾸미기
+const ServerError = () => {
+  return (
+
+      <>
+        <Header />
+        {/* FIXME: 여기 꾸며줘 
+        */}
+        <div className=''>
+            <h1>500 Page</h1>
+        </div>
+      <Footer />
+    </>
+  )
+}
+
+export default ServerError
