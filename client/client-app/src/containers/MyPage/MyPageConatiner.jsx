1eea44e7aa9cb8464d023f0fc3b50ca22d821838
kyocha_nnn <koogc0724@gmail.com>
2024-01-08 14:21:38 +0900

    0108^

--- client/client-app/src/containers/MyPage/MyPageConatiner.jsx
+++ client/client-app/src/containers/MyPage/MyPageConatiner.jsx
@@ -1,18 +1,17 @@
-import React, { useState, useEffect, useContext } from 'react'
-import MyPage from '../../components/Mypage/MyPage'
-import * as userAuth from '../../apis/users/userAuth'
-import { LoginContext } from '../../contexts/LoginContextProvider'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from "react";
+import MyPage from "../../components/Mypage/MyPage";
+import * as userAuth from "../../apis/users/userAuth";
+import { LoginContext } from "../../contexts/LoginContextProvider";
+import { useNavigate } from "react-router-dom";
 
 const MyPageConatiner = () => {
-
   const [userInfo, setUserInfo] = useState();
-  const { isLogin, roles, logout } = useContext(LoginContext)
-  const navigate = useNavigate()
+  const { isLogin, roles, logout } = useContext(LoginContext);
+  const navigate = useNavigate();
 
-  const [file, setFile] = useState(null)
-  const [fileSource, setFileSource] = useState('')
-  const [fileName, setFileName] = useState('')
+  const [file, setFile] = useState(null);
+  const [fileSource, setFileSource] = useState("");
+  const [fileName, setFileName] = useState("");
 
   // 회원 정보 조회 - /MyPage
   const getUserInfo = async () => {
@@ -22,22 +21,22 @@ const MyPageConatiner = () => {
       navigate("/login");
       return;
     }
-    
+
     if (!(roles.isUser || roles.isBand || roles.isClub)) {
       navigate("/login");
       return;
     }
 
-    const response = await userAuth.userInfo()
-    const data = response.data
-    console.log(`getUserInfo`)
-    console.log(data)
-    setUserInfo(data)
-  }
+    const response = await userAuth.userInfo();
+    const data = response.data;
+    console.log(`getUserInfo`);
+    console.log(data);
+    setUserInfo(data);
+  };
 
   useEffect(() => {
-    getUserInfo()
-  }, [])
+    getUserInfo();
+  }, []);
 
   const sets = {
     file,
@@ -46,11 +45,11 @@ const MyPageConatiner = () => {
     setFileSource,
     fileName,
     setFileName,
-  }
+  };
 
   return (
-    <MyPage isLogin={isLogin} sets={sets} userInfo={userInfo} roles={roles}/>
-  )
-}
+    <MyPage isLogin={isLogin} sets={sets} userInfo={userInfo} roles={roles} />
+  );
+};
 
-export default MyPageConatiner
+export default MyPageConatiner;
